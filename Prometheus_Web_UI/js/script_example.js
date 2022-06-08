66f3ce4ea57a33797d2d7f2dfc68dbd56046c1e9
AoDWork <88074725+AoDWork@users.noreply.github.com>
2022-06-08 15:17:44 +0300

    End of course

--- Prometheus_Web_UI/js/script_example.js
+++ Prometheus_Web_UI/js/script_example.js
@@ -1,2 +1,7 @@
-var a = 'Hello!';
-alert(a);
+(function() {
+    let child = document.createElement("li"); 
+    let text = document.createTextNode("Fifth");
+    child.appendChild(text);    
+    let parent = document.getElementById("list"); 
+    parent.appendChild(child);
+})();
